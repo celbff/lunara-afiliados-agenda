2b8663d305b69cd201f6cd4b8d5bd1fe7883473b
Cel | Lunara <celsot.holistica@gmail.com>
2025-07-25 22:50:15 -0300

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,1 +1,9 @@
+// backend/routes/api.js
+const express = require('express');
+const router = express.Router();
+const { adicionarAgendamento } = require('../controllers/agendamento');
 
+// Rota para receber agendamentos
+router.post('/agendamentos', adicionarAgendamento);
+
+module.exports = router;
